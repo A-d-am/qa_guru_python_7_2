fbc9e135ccbefb33544b28bd66e2d1a1298f398b
A-d-a-m <adam3928@mail.ru>
2023-07-27 17:02:24 +0200

    Homework

---
+++ test_homework_search.py
@@ -0,0 +1,17 @@
+from selene import browser
+from selene import be, have
+from selenium import webdriver
+
+browser.config.driver_name = 'firefox'
+
+def test_search_yashaka_github(set_browser_resolution):
+    browser.open('https://google.com')
+    browser.element('[id="APjFqb"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="rcnt"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_googl_search_stub():
+    browser.open('https://google.com')
+    input_text = 'iausdfhgoidsygfhisuadyfgdsakuyfgdsaukyfgdsaukfyasgduf'
+    browser.element('[id="APjFqb"]').type(input_text).press_enter()
+    browser.element('[id="rcnt"]').should(have.text(f'По запросу {input_text} ничего не найдено.'))
