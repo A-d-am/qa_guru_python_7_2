dd154d50f067bd4ae6998649d2e6fe1da4b6e038
A-d-a-m <adam3928@mail.ru>
2023-07-27 17:03:23 +0200

    Homework

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture
+def set_browser_resolution():
+    # browser.config.window_width("1233")
+    # browser.config.window_height("1233")
+    pass
+
+
