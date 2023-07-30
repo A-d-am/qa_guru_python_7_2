5ce55ed2f9e365f8313d8e6d16b99bee577c3690
A-d-a-m <adam3928@mail.ru>
2023-07-30 15:00:27 +0200

    Homework ver (added new locators for search field)

--- test_homework_search.py
+++ test_homework_search.py
@@ -4,12 +4,12 @@ from selene import be, have
 
 def test_search_yashaka_github(open_firefox_with_given_resolution):
     browser.open('')
-    browser.element('[id="APjFqb"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="rcnt"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    browser.element('[type="search"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="center_col"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
 def test_googl_search_stub(open_firefox_with_given_resolution):
     browser.open('')
     input_text = 'iausdfhgoidsygfhisuadyfgdsakuyfgdsaukyfgdsaukfyasgduf'
-    browser.element('[id="APjFqb"]').type(input_text).press_enter()
-    browser.element('[id="rcnt"]').should(have.text(f'По запросу {input_text} ничего не найдено.'))
+    browser.element('[type="search"]').type(input_text).press_enter()
+    browser.element('[class="card-section"]').should(have.text(f'По запросу {input_text} ничего не найдено.'))
